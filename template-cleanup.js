0818f7c2168a4ad5e5528eccf546840a41583bf6
Patrick Fisher <patrick@pwfisher.com>
2021-03-31 16:46:21 -0700

    template and its cleanup

---
+++ template-cleanup.js
@@ -0,0 +1,44 @@
+/*
+ * Docs: https://github.com/kaansoral/adventureland
+ */
+let attack_mode = true
+
+const loopTime = 250
+
+setInterval(function() {
+
+	use_hp_or_mp()
+
+	loot()
+
+	if (!attack_mode || character.rip || is_moving(character)) return
+
+	const target = getTarget()
+
+	if (!is_in_range(target)) moveHalfwayTo(target)
+	else if (can_attack(target)) {
+		set_message('Attacking')
+		attack(target)
+	}
+
+}, loopTime)
+
+function getTarget () {
+	let target = get_targeted_monster()
+	if (!target) {
+		target = get_nearest_monster({ min_xp:100, max_att:120 })
+		if (target) change_target(target)
+		else {
+			set_message('No Monsters')
+			return
+		}
+	}
+	return target
+}
+
+function moveHalfwayTo(target) {
+	move (
+		character.x + (target.x - character.x) / 2,
+		character.y + (target.y - character.y) / 2
+	)
+}
