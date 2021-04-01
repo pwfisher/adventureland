f80ef79d8bef00cd2637bfc2f875626ca10cb985
Patrick Fisher <patrick@pwfisher.com>
2021-03-31 21:08:22 -0700

    Kiting Ranger

--- ranger.js
+++ ranger.js
@@ -20,11 +20,16 @@ setInterval(function tick() {
   loot()
 
   const mob = getTargetMob()
+  if (!mob) return
 
-  if ((autoAttack && isSafeTarget(mob)) || iHaveAggro()) attack(mob)
+  if (
+    !is_on_cooldown('attack') &&
+    (iAmTargetOf(mob) || (autoAttack && isSafeTarget(mob) && distance(character, mob) > rangeMelee))
+  )
+    attack(mob)
 
   if (autoStalk) {
-    if (mob && !is_in_range(mob, 'attack')) moveToward(mob)
+    if (!is_moving(character) && !is_in_range(mob, 'attack')) moveToward(mob)
     if (is_moving(character) && distance(character, mob) < rangeStalk) stop()
   }
 
@@ -32,27 +37,20 @@ setInterval(function tick() {
 }, tickDelay)
 
 function getTargetMob() {
-  const current = get_targeted_monster()
-  if (iHaveAggro()) return current
+  const aggroMob = get_nearest_monster({ target: character })
+  if (aggroMob) return aggroMob
 
   const mob = get_nearest_monster({ min_xp: 1, max_att: mobAtkMax })
   if (isSafeTarget(mob)) {
     change_target(mob)
     return mob
   }
-  return current
+
+  return get_targeted_monster()
 }
 
-const iHaveAggro = () => get_targeted_monster()?.target === character.id
-
-function isSafeTarget(mob) {
-  return can_attack(mob) && mob.attack < mobAtkMax && distance(character, mob) > rangeMelee
-}
-
-function moveToward(target) {
-  move(character.x + (target.x - character.x) / 2, character.y + (target.y - character.y) / 2)
-}
-
-function moveUntoward(target) {
-  move(character.x - (target.x - character.x), character.y - (target.y - character.y))
-}
+const iAmTargetOf = x => x.target === character.id
+const isSafeTarget = x => x.attack < mobAtkMax
+const moveToward = p =>
+  move(character.x + (p.x - character.x) / 2, character.y + (p.y - character.y) / 2)
+const moveUntoward = p => move(character.x - (p.x - character.x), character.y - (p.y - character.y))
