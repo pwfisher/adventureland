3b952e292391e7cd8ef8dd89a93e09b04611bc25
Patrick Fisher <patrick@pwfisher.com>
2021-03-31 18:29:44 -0700

    tabs to spaces and some evolution

--- template-cleanup.js
+++ template-cleanup.js
@@ -5,37 +5,32 @@ let attack_mode = true
 
 const loopTime = 250
 
-setInterval(function() {
+setInterval(function () {
+  use_hp_or_mp()
 
-	use_hp_or_mp()
+  loot()
 
-	loot()
+  if (!attack_mode || character.rip || is_moving(character)) return
 
-	if (!attack_mode || character.rip || is_moving(character)) return
+  const target = getTarget()
 
-	const target = getTarget()
-
-	if (!is_in_range(target)) moveHalfwayTo(target)
-	else if (can_attack(target)) {
-		set_message('Attacking')
-		attack(target)
-	}
-
+  if (!is_in_range(target)) moveHalfwayTo(target)
+  else if (can_attack(target)) {
+    set_message('Attacking')
+    attack(target)
+  }
 }, loopTime)
 
-function getTarget () {
-	let target = get_targeted_monster()
-	if (!target) {
-		target = get_nearest_monster({ min_xp: 100, max_att: 120 })
-		if (target) change_target(target)
-		else set_message('No Monsters')
-	}
-	return target
+function getTarget() {
+  let target = get_targeted_monster()
+  if (!target) {
+    target = get_nearest_monster({ min_xp: 100, max_att: 120 })
+    if (target) change_target(target)
+    else set_message('No Monsters')
+  }
+  return target
 }
 
 function moveHalfwayTo(target) {
-	move (
-		character.x + (target.x - character.x) / 2,
-		character.y + (target.y - character.y) / 2
-	)
+  move(character.x + (target.x - character.x) / 2, character.y + (target.y - character.y) / 2)
 }
